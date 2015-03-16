d785861027c7ba49173dc27b1f9babb6e296704b
unknown <joao.amorim@zappa.cmw.local>
2015-03-16 09:44:58 +0000

    Create BD Init

---
+++ Planeamento/Primeiro Plano/BDInit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planeamento.Primeiro_Plano
+{
+    class BDInit
+    {
+        private SqlConnection connection;
+        private String SQL;
+
+        public BDInit() {
+
+        }
+    }
+}
